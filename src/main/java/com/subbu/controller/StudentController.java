5c95c8b2e16ce306ef83b85cb503c42de3f6fdb1
ganesh77991 <ganeshpadala126@gmail.com>
2020-08-24 19:23:54 +0530

    add update and delete

--- src/main/java/com/subbu/controller/StudentController.java
+++ src/main/java/com/subbu/controller/StudentController.java
@@ -1,9 +1,14 @@
 package com.subbu.controller;
 
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +23,38 @@ public class StudentController {
 	@Autowired
 	private StudentService studentService;
 
-	@PostMapping(name = "save")
+	@PostMapping(value = "/save")
 	public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
 
 		Student stu = studentService.saveStudent(student);
 
 		return new ResponseEntity<Student>(stu, HttpStatus.CREATED);
 	}
+
+	@DeleteMapping(value = "/delete/{id}")
+	public ResponseEntity deleteStudent(@PathVariable Integer id) {
+
+		studentService.deleteStudent(id);
+
+		return new ResponseEntity("Student Delete Successfully", HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/get/{id}")
+	public ResponseEntity<Student> getStudentById(@PathVariable Integer id) {
+
+		Student student = studentService.findStudentById(id);
+
+		return new ResponseEntity<Student>(student, HttpStatus.OK);
+	}
+
+	@PutMapping(value = "/update/{id}")
+	public ResponseEntity<Student> updateStudnet(@PathVariable Integer id, @RequestBody Student student) {
+
+		Student stud = studentService.findStudentById(id);
+		Student student1 = studentService.updateStudnet(stud,id);
+		
+
+		return new ResponseEntity<Student>(student1, HttpStatus.OK);
+	}
+
 }
--- src/main/java/com/subbu/service/StudentService.java
+++ src/main/java/com/subbu/service/StudentService.java
@@ -14,7 +14,57 @@ public class StudentService {
 
 	public Student saveStudent(Student student) {
 
-	return	studentJpa.save(student);
+		return studentJpa.save(student);
+	}
+
+	public void deleteStudent(Integer studentId) {
+
+		try {
+
+			Student student = studentJpa.findById(studentId).get();
+
+			if (student != null) {
+				System.out.println("DeletIng  Student  Details.. " + student);
+				studentJpa.delete(student);
+			}
+		} catch (Exception e) {
+			System.out.println("Student Not Found....");
+		}
+
+	}
+
+	public Student findStudentById(Integer id) {
+
+		Student student = null;
+		try {
+
+			student = studentJpa.findById(id).get();
+
+			if (student != null) {
+				System.out.println("DeletIng  Student  Details.. " + student);
+				studentJpa.delete(student);
+			}
+		} catch (Exception e) {
+			System.out.println("Student Not Found....");
+		}
+		return student;
+	}
+
+	public Student updateStudnet(Student stu, Integer id) {
+		// Student student = null;
+		try {
+
+			// student = studentJpa.findById(id).get();
+
+			if (stu != null) {
+				System.out.println("DeletIng  Student  Details.. " + stu);
+
+				studentJpa.save(stu);
+			}
+		} catch (Exception e) {
+			System.out.println("Student Not Found....");
+		}
+		return stu;
 	}
 
 }
